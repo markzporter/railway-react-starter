606f4cc668cec414b1cd043d3913bc3494c199e4
Mark Porter <mark.porter@gbatteries.com>
2025-01-26 19:06:29 -0500

    fix template literal

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>Choo Choo! This is an example of a create-react-app site running on Railway.</p>
 
-        <p>f'{process.env.REACT_APP_API_KEY}'</p>
+        <p>{`API KEY is ${process.env.REACT_APP_API_KEY}`}</p>
         <a className="App-link" href="https://react.dev/learn" target="_blank" rel="noreferrer noopener">Learn React</a>
       </header>
     </div>
